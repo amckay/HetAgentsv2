2fac306b4eb0811b7430cb7499a08b73273749b3
Alisdair McKay <alisdair.mckay@gmail.com>
2024-08-30 08:50:40 -0500

    Bug fixes in SSJac.jl

--- Codes/SSJac.jl
+++ Codes/SSJac.jl
@@ -3,7 +3,7 @@ include("HA_fcns.jl")
 include("HA_setup.jl")
 
 
-compute_ss!(ss);
+compute_ss!(ss,par,grid);
 
 
 
@@ -12,16 +12,16 @@ compute_ss!(ss);
 
 T = 200;
 print("Starting fake news...")
-J = fakenews(ss,T)
+J = fakenews(ss,T,par,grid)
 println("done")
 
 
 print("Starting numerical Jacobian columns...")
-nm_W = numerical_jac(ss,T,1,10);
-nm_R = numerical_jac(ss,T,2,10);
+nm_W = numerical_jac(ss,T,1,10,par,grid);
+nm_R = numerical_jac(ss,T,2,10,par,grid);
 println("done")
 
 p_W = plot([J[1:100,10,1,1] nm_W[1:100]], labels=["Fake news" "Numerical"], width=3, title="W", titlefontsize=12,linestyles=[:solid :dash])
 p_R = plot([J[1:100,10,1,2] nm_R[1:100]], labels=["Fake news" "Numerical"], width=3, title="R", titlefontsize=12,linestyles=[:solid :dash])
 plot(p_W, p_R)
-savefig("Figures/SSJ.png")
+savefig("Notes/Figures/SSJ.png")
